b759bcfc035f7059135e888250b3570f09728563
romanenkoio <61626669+romanenkoio@users.noreply.github.com>
2023-06-06 00:06:41 +0300

    init commit

---
+++ Garage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Garage
+//
+//  Created by Illia Romanenko on 6.06.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
