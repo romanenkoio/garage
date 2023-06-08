8e0a5240afe3944b626dbf04687f91dbb3c2aa4e
romanenkoio <61626669+romanenkoio@users.noreply.github.com>
2023-06-08 12:22:13 +0300

    update

--- Garage/UIKit/CellViews/ServiceView/ServiceView.swift
+++ Garage/UIKit/CellViews/ServiceView/ServiceView.swift
@@ -24,4 +24,31 @@ class ServiceView: BasicView {
         let label = BasicLabel()
         return label
     }()
+    
+    private var adressLabel: BasicLabel = {
+        let label = BasicLabel()
+        return label
+    }()
+    
+    private var specializationLabel: BasicLabel = {
+        let label = BasicLabel()
+        return label
+    }()
+    
+    override func initView() {
+        makeLayout()
+        makeConstraint()
+    }
+    
+    private func makeLayout() {
+        
+    }
+    
+    private func makeConstraint() {
+        
+    }
+    
+    func setViewModel(_ vm: ViewModel) {
+        
+    }
 }
