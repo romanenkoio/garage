ba1e5d8234156faf329fec4f818ab2b26e5c9957
romanenkoio <61626669+romanenkoio@users.noreply.github.com>
2023-06-10 02:07:26 +0300

    cleanup

--- Garage/Feature/CreateCar/ViewController/CreateCar+ViewModel.swift
+++ Garage/Feature/CreateCar/ViewController/CreateCar+ViewModel.swift
@@ -192,6 +192,7 @@ extension CreateCarViewController {
         func decodeVIN() {
             Task { @MainActor in
                 do {
+                    guard !self.winFieldVM.text.isEmpty else { return }
                     let result = try await NetworkManager
                         .sh
                         .request(
