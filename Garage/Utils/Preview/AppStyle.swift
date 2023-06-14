6562f566d292c79d31f04b41c80c2470695d6df5
romanenkoio <61626669+romanenkoio@users.noreply.github.com>
2023-06-14 16:34:20 +0300

    update app style

--- Garage/Utils/Preview/AppStyle.swift
+++ Garage/Utils/Preview/AppStyle.swift
@@ -7,11 +7,23 @@
 
 import Foundation
 
-enum AppStyle {
+enum AppStyle: String {
     case black
-    case blue
+    case standartBlue
     case orange
     case darkBlue
-    case mins
+    case mint
     case lightBlue
+    
+    init(rawValue: String) {
+        switch rawValue {
+        case AppStyle.black.rawValue:           self = .black
+        case AppStyle.standartBlue.rawValue:    self = .standartBlue
+        case AppStyle.orange.rawValue:          self = .orange
+        case AppStyle.darkBlue.rawValue:        self = .darkBlue
+        case AppStyle.mint.rawValue:            self = .mint
+        case AppStyle.lightBlue.rawValue:       self = .lightBlue
+        default:                                self = .standartBlue
+        }
+    }
 }
