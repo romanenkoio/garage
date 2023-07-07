609a9d6d510f9ac6f57a21cf5dbc745f57a42a04
romanenkoio <61626669+romanenkoio@users.noreply.github.com>
2023-07-08 00:24:31 +0300

    update reminder logic

--- Garage/Models/Realm/Reminder.swift
+++ Garage/Models/Realm/Reminder.swift
@@ -14,6 +14,7 @@ final class Reminder: Object {
     @Persisted var comment: String?
     @Persisted var carID: String
     @Persisted var date: Date
+    @Persisted var isDone: Bool
     
     var days: Int? {
         Date().daysBetween(date: date)
@@ -31,9 +32,31 @@ final class Reminder: Object {
         self.comment = comment
         self.carID = carID
         self.date = date
+        self.isDone = false
     }
     
     func setPush() {
         PushManager.sh.create(LocalPush(reminder: self))
+    }
+    
+    func completeReminder() {
+        PushManager.sh.removePush(self.id)
+        RealmManager().update { [weak self] realm in
+            try? realm.write({ [weak self] in
+                guard let self else { return }
+                self.isDone = true
+            })
+        }
+    }
+    
+    func uncompleteReminder() {
+        PushManager.sh.removePush(self.id)
+        RealmManager().update { [weak self] realm in
+            try? realm.write({ [weak self] in
+                guard let self else { return }
+                self.isDone = false
+                self.setPush()
+            })
+        }
     }
 }
